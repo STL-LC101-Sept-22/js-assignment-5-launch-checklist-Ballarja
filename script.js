3afb81c2600cd477cd9b694ef27faaf6e542f2ab
Ballarja <JamesBallard87@hotmail.com>
2022-11-30 14:10:22 -0600

    added let listedPlanetsResponse = myFetch();

--- script.js
+++ script.js
@@ -25,10 +25,11 @@ window.addEventListener("load", function() {
       });
     let listedPlanets;
     // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-    let listedPlanetsResponse;
+    let listedPlanetsResponse = myFetch();
+    
     listedPlanetsResponse.then(function (result) {
         listedPlanets = result;
-    
+        
         console.log(listedPlanets);
     }).then(function () {
         console.log(listedPlanets);
